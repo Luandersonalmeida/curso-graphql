4af5d4a99ea829bd9e7fd4033116dc8890d6bd18
Luanderson Almeida <ls_luanderson@hotmail.com>
2023-09-25 22:42:14 -0300

    primeiros resolvers no apollo server (GraphQL)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import { ApolloServer, gql } from "apollo-server";
 const server = new ApolloServer({
   typeDefs: gql`
   type Query {
-    hello: String
+    hello: String,
+    hi: String
   }
   `,
+  resolvers: {
+    Query: {
+      hello: async () => {
+        return 'retorna string';
+      },
+      hi: () => {
+        return 'oi';
+      },
+    }
+  }
 });
 
 server.listen(4003).then(({ url }) => {
-  console.log(`server listeningon url ${url}`)
+  console.log(`server listening on url ${url}`)
 })
